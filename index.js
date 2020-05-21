9e8daa383d5275a0b0f9c6255744a85cf257b25a
Non <neiooz590@gmail.com>
2020-05-22 01:36:44 +0200

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.login('Njk2NDg3OTU3MTY2Njg2MjI4.XscJVg.Cjfn-cH2tyXpU0mhi_6KzFWegPE');
+
+client.on('ready', () => {
+console.log ('connecter');
+})
