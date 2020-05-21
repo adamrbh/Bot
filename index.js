a7752cae27a6872b736ee075675545504ad3e6f6
Non <neiooz590@gmail.com>
2020-05-22 01:55:28 +0200

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.login('Njk2NDg3OTU3MTY2Njg2MjI4.XscJVg.Cjfn-cH2tyXpU0mhi_6KzFWegPE');
+client.login(process.env.BOT_TOKEN)
 
 client.on('ready', () => {
 console.log ('connecter');
